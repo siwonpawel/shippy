512d998872bc09cca2d0b278d7a67ce4de27aa91
siwonpawel <siwonpawel@gmail.com>
2020-12-22 21:44:39 +0100

    implement micro for shippy-cli

--- shippy-cli-consignment/go.mod
+++ shippy-cli-consignment/go.mod
@@ -1,10 +1,11 @@
-module github.com/siwonpawel/shippy/shippy-cli-consignment
+module github.com/EwanValentine/shippy-cli-consignment
 
 go 1.15
 
-require (
-	github.com/siwonpawel/shippy/shippy-service-consignment v0.0.0-00010101000000-000000000000
-	google.golang.org/grpc v1.34.0
-)
-
 replace github.com/siwonpawel/shippy/shippy-service-consignment => ../shippy-service-consignment
+replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
+
+require (
+	github.com/micro/go-micro/v2 v2.9.1
+	github.com/siwonpawel/shippy/shippy-service-consignment v0.0.0-20201221192730-00d93b578765
+)
--- shippy-cli-consignment/main.go
+++ shippy-cli-consignment/main.go
@@ -7,8 +7,8 @@ import (
 	"log"
 	"os"
 
+	micro "github.com/micro/go-micro/v2"
 	pb "github.com/siwonpawel/shippy/shippy-service-consignment/proto/consignment"
-	"google.golang.org/grpc"
 )
 
 const (
@@ -30,26 +30,27 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
 
+	service := micro.NewService(micro.Name("shippy.cli.consignment"))
+	service.Init()
+
+	client := pb.NewShippingService("shippy.service.consignment", service.Client())
+
+	// Contact the server and print out its response.
 	file := defaultFilename
 	if len(os.Args) > 1 {
 		file = os.Args[1]
 	}
 
 	consignment, err := parseFile(file)
+
 	if err != nil {
-		log.Fatalf("could not parse the file: %v", err)
+		log.Fatalf("Could not parse file: %v", err)
 	}
 
 	r, err := client.CreateConsignment(context.Background(), consignment)
 	if err != nil {
-		log.Fatalf("Could not greet: %v", err)
+		log.Fatalf("Could not create a consignment: %v", err)
 	}
 	log.Printf("Created: %t", r.Created)
 
@@ -57,7 +58,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not list consignments: %v", err)
 	}
+
 	for _, v := range getAll.Consignments {
 		log.Println(v)
 	}
+
 }
